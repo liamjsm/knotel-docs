470d9a2ae634e4c81da3ec6ab0d313f6ecfa57ce
eluck <mail@eluck.me>
2017-04-26 23:48:44 +0300

    Try caching node_modules with yarn

--- scripts/deploy-ghpages.sh
+++ scripts/deploy-ghpages.sh
@@ -7,6 +7,5 @@ git config --global user.name "CircleCI"
 
 cd webapp
 
-npm install
 MESSAGE="CircleCI[$CIRCLE_BUILD_NUM]:$CIRCLE_SHA1@$CIRCLE_BRANCH[ci skip]"
 COMMIT_MESSAGE="$MESSAGE" npm run deploy
